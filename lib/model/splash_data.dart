7ce68abe86f75baa411af59e8f06ebbca2d8ff4a
alezunazikri <pohanzikri2@gmail.com>
2024-10-16 09:33:20 +0700

    first commit

---
+++ lib/model/splash_data.dart
@@ -0,0 +1,14 @@
+List<Map<String, String>> splashData = [
+  {
+    "text": "Welcome to My Toko, your \nshopping adventure starts here!",
+    "image": "assets/images/splash_1.png"
+  },
+  {
+    "text": "Connecting you with stores and \nproducts from all over the world.",
+    "image": "assets/images/splash_2.png"
+  },
+  {
+    "text": "Shopping made easy. Relax at \nhome, we've got you covered.",
+    "image": "assets/images/splash_3.png"
+  },
+];
